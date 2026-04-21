3cefc22351126b081c040a8367b1d94084477828
mosait <mohamad-saleh-@hotmail.de>
2026-04-21 18:14:33 +0200

    modify dynamicColor in EmsTheme; add night theme resources

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "ems.aab"
         minSdk = 29
         targetSdk = 36
-        versionCode = 5
-        versionName = "0.1.2"
+        versionCode = 8
+        versionName = "0.1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         ndk {
--- core/ui/src/main/java/com/mosait/ems/core/ui/theme/Theme.kt
+++ core/ui/src/main/java/com/mosait/ems/core/ui/theme/Theme.kt
@@ -68,7 +68,7 @@ private val DarkColorScheme = darkColorScheme(
 @Composable
 fun EmsTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
